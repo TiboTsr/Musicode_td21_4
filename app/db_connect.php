ce71d39559a34cab014c4e47cabe76a59f2de43b
grandieres Florian <florian.grandieres@gmail.com>
2025-12-01 12:03:49 +0100

    RespectSujet

--- app/db_connect.php
+++ app/db_connect.php
@@ -1,14 +1,18 @@
 <?php
-$db_host = 'localhost';
-$db_name = 'musicode';
-$db_user = 'root';
-$db_pass = '';
+require_once __DIR__ . '/../vendor/autoload.php';
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+$db_host = $_ENV['DB_HOST'];
+$db_name = $_ENV['DB_NAME'];
+$db_user = $_ENV['DB_USER'];
+$db_pass = $_ENV['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
+    die("Erreur de connexion : " . $e->getMessage());
 }
 ?>
