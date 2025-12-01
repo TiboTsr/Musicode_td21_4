6108be5f102bcd9c3b44476d9fd411d87f4726aa
grandieres Florian <florian.grandieres@gmail.com>
2025-12-01 11:02:54 +0100

    CreationdeCompteFonctionne

--- app/db_connect.php
+++ app/db_connect.php
@@ -1,11 +1,11 @@
 <?php
-$host = 'localhost';
-$dbname = 'musicode';
-$username = 'root';
-$password = '';
+$db_host = 'localhost';
+$db_name = 'musicode';
+$db_user = 'root';
+$db_pass = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
